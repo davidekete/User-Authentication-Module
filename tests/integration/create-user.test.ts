4484fd63413c67d90bfa7667cb130a714a360b1d
davidekete <eketedavid@gmail.com>
2023-05-28 20:51:43 +0100

    Remove mocks from integration tests

--- tests/integration/create-user.test.ts
+++ tests/integration/create-user.test.ts
@@ -3,25 +3,15 @@ import app from '../../src/index';
 import { User } from '../../src/database/models/user.model';
 
 describe('Create New User', () => {
-  const user = {
-    id: 1,
-    username: 'testuser0',
-    firstname: 'test',
-    lastname: 'user',
-    email: 'testuser0@example.com',
-    password: '#Test1234',
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-  };
-
   test('It should create a new user', async () => {
-    const mockCreateUser = jest.fn((): any => user);
+    const res = await request(app).post('/api/signup').send({
+      username: 'testuser0',
+      firstname: 'test',
+      lastname: 'user',
+      email: 'testuser0@example.com',
+      password: '#Test1234',
+    });
 
-    jest.spyOn(User, 'create').mockImplementation(() => mockCreateUser());
-
-    const res = await request(app).post('/api/signup').send(user);
-
-    expect(mockCreateUser).toHaveBeenCalled();
     expect(res.body.newUser).toHaveProperty('id');
     expect(res.body.newUser).toHaveProperty('updatedAt');
     expect(res.body.newUser).toHaveProperty('createdAt');
@@ -29,14 +19,7 @@ describe('Create New User', () => {
   });
 
   test('It should handle exceptions', async () => {
-    const mockCreateUser = jest.fn((): any => {
-      throw 'error';
-    });
-
-    jest.spyOn(User, 'create').mockImplementation(() => mockCreateUser());
-    const res = await request(app).post('/api/signup').send(user);
-
-    expect(mockCreateUser).toHaveBeenCalled();
+    const res = await request(app).post('/api/signup').send({});
     expect(res.body).toHaveProperty('error');
     expect(res.statusCode).toBe(500);
   });
@@ -48,6 +31,4 @@ describe('Create New User', () => {
       error: '"username" is required',
     });
   });
-
-  
 });
